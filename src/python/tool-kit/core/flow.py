7d3135d7f71c56341a1056fc3d5090b47c24496e
jcheeks7 <jcheeks7@gatech.edu>
2025-06-11 11:58:00 -0400

    Update and rename flowy.py to flow.py
    Update equations

--- src/python/tool-kit/core/flow.py
+++ src/python/tool-kit/core/flow.py
@@ -7,7 +7,6 @@ def estimate_mass_flow_rate(P0, T0, A_throat, gamma, R_specific):
     pressure_term = P0 / sqrt(T0)
     flow_term = sqrt(gamma / R_specific)
     expansion_term = ((gamma + 1) / 2) ** (- (gamma + 1) / (2 * (gamma - 1)))
-    
     return A_throat * pressure_term * flow_term * expansion_term
 
 def area_mach_eq(M, expansion_ratio, gamma):
@@ -21,3 +20,16 @@ def exit_mach(expansion_ratio, gamma):
     M_e, = fsolve(area_mach_eq, guess, args=(expansion_ratio, gamma))
     return M_e
 
+def exit_temperature(T_t, M_e, gamma):
+    T_e = T_t / (1 + ((gamma - 1) / 2) * M_e**2)
+    return T_e
+
+def exit_pressure(P_t, M_e, gamma):
+    P_e = P_t / (1 + ((gamma - 1) / 2) * M_e**2) ** (gamma / (gamma - 1))
+    return P_e
+from math import sqrt
+
+def exit_velocity(T_e, gamma, R_specific, M_e):
+    a_e = sqrt(gamma * R_specific * T_e)  # Speed of sound at exit
+    v_e = M_e * a_e
+    return v_e
