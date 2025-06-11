c6ce6c148cd93186b8de46f3257ff8027b2bdb47
jcheeks7 <jcheeks7@gatech.edu>
2025-06-11 11:00:18 -0400

    Update sizing.py
    Added equations to calculate total impulse, propellant required, tank volume, and delta-V

--- src/python/tool-kit/core/sizing.py
+++ src/python/tool-kit/core/sizing.py
@@ -1,1 +1,19 @@
+from .constants import g0
+
+def calculate_total_impulse(thrust, burn_time):
+    "Total impulse [N·s] given thrust [N] and burn time [s]."
+    return thrust * burn_time
+
+def calculate_required_propellant(I_total, Isp):
+    "Required propellant mass [kg] given total impulse [N·s] and Isp [s]."
+    return I_total / (Isp * g0)
+
+def calculate_tank_volume(m_propellant, propellant_density):
+    "Tank volume [m³] given propellant mass [kg] and density [kg/m³]."
+    return m_propellant / propellant_density
+
+def calculate_delta_v(Isp, m_initial, m_final):
+    "Delta-v [m/s] using rocket equation."
+    from math import log
+    return Isp * g0 * log(m_initial / m_final)
 
