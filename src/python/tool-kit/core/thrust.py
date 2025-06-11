962144d744d46a224fdab3609996a4dd7aa396a6
jcheeks7 <jcheeks7@gatech.edu>
2025-06-11 13:31:21 -0400

    Update thrust.py
    Added equations

--- src/python/tool-kit/core/thrust.py
+++ src/python/tool-kit/core/thrust.py
@@ -1,1 +1,10 @@
+from .constants import P_0
+from .constants import g0
 
+def calculate_thrust(m_dot, v_e, P_e, A_exit):
+    momentum = m_dot * v_e
+    pressure = (P_e - P_0) * A_exit
+    return momentum + pressure
+
+def calculate_isp(v_e):
+    return v_e / g0
