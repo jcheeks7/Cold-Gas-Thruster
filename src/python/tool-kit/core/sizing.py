e73c3eb4c534d4d2de8ca259c0a7c596b3c4ec84
jcheeks7 <jcheeks7@gatech.edu>
2025-06-11 13:40:12 -0400

    Update sizing.py
    Populated equations

--- src/python/tool-kit/core/sizing.py
+++ src/python/tool-kit/core/sizing.py
@@ -1,18 +1,49 @@
 from .constants import g0
-from math import log
 
 def calculate_total_impulse(thrust, burn_time):
-    "Total impulse [N·s] given thrust [N] and burn time [s]."
+    """
+    Parameters:
+    - thrust [N]
+    - burn_time [s]
+
+    Returns:
+    - Total impulse [N·s]
+    """
     return thrust * burn_time
 
+
 def calculate_required_propellant(I_total, Isp):
-    "Required propellant mass [kg] given total impulse [N·s] and Isp [s]."
+    """
+    Parameters:
+    - I_total [N·s]
+    - Isp [s]
+
+    Returns:
+    - Propellant mass [kg]
+    """
     return I_total / (Isp * g0)
 
-def calculate_tank_volume(m_propellant, propellant_density):
-    "Tank volume [m³] given propellant mass [kg] and density [kg/m³]."
-    return m_propellant / propellant_density
+def calculate_tank_volume(m_propellant, density):
+    """
+    Parameters:
+    - m_propellant [kg]
+    - density [kg/m³]
+
+    Returns:
+    - Volume [m³]
+    """
+    return m_propellant / density
+
+from math import log
 
 def calculate_delta_v(Isp, m_initial, m_final):
-    "Delta-v [m/s] using rocket equation."
+    """
+    Parameters:
+    - Isp [s]
+    - m_initial [kg]
+    - m_final [kg]
+
+    Returns:
+    - Delta-v [m/s]
+    """
     return Isp * g0 * log(m_initial / m_final)
