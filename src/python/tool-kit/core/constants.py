9ae2dd1cc33ff94bd28c15f8d43658530428cae6
jcheeks7 <jcheeks7@gatech.edu>
2025-06-11 11:40:47 -0400

    Update constants.py
    Updating and adding new constants. Uniform format

--- src/python/tool-kit/core/constants.py
+++ src/python/tool-kit/core/constants.py
@@ -1,1 +1,15 @@
-g0 = 9.80665  # Standard gravity in m/s^2
+# ---- Physical Constants ----
+g0 = 9.80665  # This is standard gravity in units [m/s^2]
+
+# ---- Gas Properties (CO₂ for this system) ----
+gamma = 1.29            # [-] Specific heat ratio for CO₂
+R_specific = 188.9      # [J/kg·K] Specific gas constant for CO₂
+
+# ---- System Conditions ----
+P_t = 5.5e6             # [Pa] Total tank pressure (paintball is ~800 psi)
+T_t = 300               # [K] Total tank temperature (doinh this at room temp, can change to reflect test environment temp)
+P_0 = 0                 # [Pa] Ambient pressure (vacuum for space, [test day pressure] for test environment)
+
+# ---- Geometry ----
+d_throat = 1e-3         # [m] Throat diameter (2mm for this)
+A_throat = 3.1416 * (d_throat/2)**2  # [m²] Throat area
